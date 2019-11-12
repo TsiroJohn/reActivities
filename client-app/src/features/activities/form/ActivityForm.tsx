2cf673d0dea81a122a2b7646d369590811d5665c
Thanos <thanos.tsirog@projectviewerp.com>
2019-11-12 20:10:19 +0200

    add agent.ts as a handler for api requests from axios

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
     setEditMode: (editMode: boolean) => void;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean
 }
 
 
@@ -15,7 +16,8 @@ export const ActivityForm: React.FC<IProps> = ({
     setEditMode,
     activity: initialFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
 
     const initializeForm = () => {
@@ -65,7 +67,7 @@ export const ActivityForm: React.FC<IProps> = ({
                 <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date} />
                 <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
                 <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
-                <Button floated='right' positive type='submit' content='Submit'></Button>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'></Button>
                 <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel'></Button>
             </Form>
         </Segment>
