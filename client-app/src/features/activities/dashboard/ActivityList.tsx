0418652e6b555af400bb0370271ac5b9a9f85ea1
Thanos <thanos.tsirog@projectviewerp.com>
2019-11-15 00:12:21 +0200

    refactoring activities adding Mobx for state management

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,56 +1,59 @@
-import React, { SyntheticEvent } from 'react'
-import { Item, Button, Label, Segment } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import React, { SyntheticEvent, useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
+
 interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    submitting: boolean;
-    target: string;
+  deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean;
+  target: string;
 }
 
-export const ActivityList: React.FC<IProps> = ({
-    activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-    target
+const ActivityList: React.FC<IProps> = ({
+   deleteActivity,
+  submitting,
+  target
 }) => {
-    return (
-        <Segment clearing>
+    const activityStore = useContext(ActivityStore);
+    const {activitiesByDate, selectActivity} = activityStore;
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {activitiesByDate.map(activity => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
+                <Button
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+};
 
-
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                                {/* <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' /> */}
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)}
-                                    floated='right'
-                                    content='View'
-                                    color='blue' />
-                                <Button name={activity.id}
-                                    loading={target === activity.id && submitting}
-                                    onClick={(e) => deleteActivity(e, activity.id)}
-                                    floated='right'
-                                    content='Delete'
-                                    color='red' />
-                                <Label basic content={activity.category}></Label>
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-
-            </Item.Group>
-        </Segment>
-    )
-}
+export default observer(ActivityList);
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,76 +1,114 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, FormEvent, useContext } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import {v4 as uuid} from 'uuid';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
-    activity: IActivity;
-    setEditMode: (editMode: boolean) => void;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean
+  setEditMode: (editMode: boolean) => void;
+  activity: IActivity;
+  editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
-
-export const ActivityForm: React.FC<IProps> = ({
-    setEditMode,
-    activity: initialFormState,
-    createActivity,
-    editActivity,
-    submitting
+const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormState,
+  editActivity,
+  submitting
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {createActivity} = activityStore;
+  const initializeForm = () => {
 
-    const initializeForm = () => {
-        if (initialFormState) {
-            return initialFormState
-        }
-        else {
-            return {
-                id: '',
-                title: '',
-                category: '',
-                description: '',
-                date: '',
-                city: '',
-                venue: ''
-            };
-        }
-    };
-    const [activity, setActivity] = useState<IActivity>(initializeForm);
-
-
-    const handleSubmit = () => {
-        if (activity.id.length === 0) 
-        {
-            let newActivity = {
-                ...activity,
-                id: uuid()
-
-            }
-            createActivity(newActivity);
-        } else {
-            editActivity(activity);
-        }
+    if (initialFormState) {
+      return initialFormState;
+    } else {
+      return {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: ''
+      };
     }
+  };
 
-    const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = event.currentTarget;
-        setActivity({ ...activity, [name]: value })
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+
+  const handleSubmit = () => {
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      };
+      activityStore.createActivity(newActivity);
+    } else {
+      editActivity(activity); 
     }
+  };
 
-    return (
-        <Segment clearing>
-            <Form onSubmit={handleSubmit}>
-                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
-                <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description} />
-                <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
-                <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date} />
-                <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
-                <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit'></Button>
-                <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel'></Button>
-            </Form>
-        </Segment>
-    )
-}
+  const handleInputChange = (
+    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.currentTarget;
+    setActivity({ ...activity, [name]: value });
+  };
 
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input
+          onChange={handleInputChange}
+          name='title'
+          placeholder='Title'
+          value={activity.title}
+        />
+        <Form.TextArea
+          onChange={handleInputChange}
+          name='description'
+          rows={2}
+          placeholder='Description'
+          value={activity.description}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name='category'
+          placeholder='Category'
+          value={activity.category}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name='date'
+          type='datetime-local'
+          placeholder='Date'
+          value={activity.date}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name='city'
+          placeholder='City'
+          value={activity.city}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name='venue'
+          placeholder='Venue'
+          value={activity.venue}
+        />
+        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+        <Button
+          onClick={() => setEditMode(false)}
+          floated='right'
+          type='button'
+          content='Cancel'
+        />
+      </Form>
+    </Segment>
+  );
+};
+
+export default observer(ActivityForm);
